b386fd62abd37476335f04f471c16a7fef440ce5
Ankit Kumar <30922723+ankgitk@users.noreply.github.com>
2019-09-16 11:14:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,37 +3,16 @@ export default {
     app: {
         gateway: "https://titanic-explainer.gateway.dialogflow.cloud.ushakov.co", // <- enter your gateway URL here, the function is just a helper function for my cloud integration. You don't normally need it
         muted: false, // <- mute microphone at start
-        start_suggestions: ["Get Started"], // <- array of suggestions, displayed at the start screen
+        start_suggestions: ["Product categories", "Bestselling product","Saved forms", "Open an account"], // <- array of suggestions, displayed at the start screen
         fallback_lang: 'en' // <- fallback language code, if history mode or network is unavailable
     },
     i18n: {
         en: {
             welcomeTitle: "Welcome to",
             muteTitle: "Mute Toggle",
-            inputTitle: "Type your message",
+            inputTitle: "Type something",
             sendTitle: "Send",
             microphoneTitle: "Voice Input"
-        },
-        ru: {
-            welcomeTitle: "Добро пожаловать в",
-            muteTitle: "Режим звука",
-            inputTitle: "Введите ваше сообщение",
-            sendTitle: "Отправить",
-            microphoneTitle: "Голосовой ввод"
-        },
-        de: {
-            welcomeTitle: "Wilkommen bei",
-            muteTitle: "Stumm Modus",
-            inputTitle: "Schreiben Sie ihre Nachricht",
-            sendTitle: "Senden",
-            microphoneTitle: "Spracheingabe"
-        },
-        fr: {
-            welcomeTitle: "Bienvenue à",
-            muteTitle: "Sound Mode",
-            inputTitle: "Entrez votre message",
-            sendTitle: "Envoyer",
-            microphoneTitle: "Entrée vocale"
         }
     }
 }
